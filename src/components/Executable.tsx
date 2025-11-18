32685e473fda1f5c928b4f7dd9c72066d4ecbc38
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-11-18 23:19:35 +0100

    feat(executable): display window with close button

--- src/components/Executable.tsx
+++ src/components/Executable.tsx
@@ -14,18 +14,23 @@ function Executable(props: Props) {
   const [executableVisible, setExecutableVisible] = useState(false);
 
   return (
-    <div
-      className="flex flex-col items-center px-4 py-1 border border-transparent hover:bg-gray-600 hover:border hover:border-white"
-      onDoubleClick={() => setExecutableVisible(true)}
-    >
-      <img className="h-12" draggable="false" src={props.icon} />
-      <p className="mt-1 text-xs text-center max-w-[80px] break-words">
-        {TextHelper.cutText(props.name, 25)}
-      </p>
+    <>
+      <div
+        className="flex flex-col items-center px-4 py-1 border border-transparent hover:bg-gray-600 hover:border hover:border-white"
+        onDoubleClick={() => setExecutableVisible(true)}
+      >
+        <img className="h-12" draggable="false" src={props.icon} />
+        <p className="mt-1 text-xs text-center max-w-[80px] break-words">
+          {TextHelper.cutText(props.name, 25)}
+        </p>
+      </div>
       {executableVisible ? (
-        <p className="absolute left-0 top-1/2">Test</p>
+        <div className="absolute top-1/4 left-1/4 bg-gray-700 p-2 w-[600px] h-[400px]">
+          <p>Application : {props.name}</p>
+          <button onClick={() => setExecutableVisible(false)}>Close me</button>
+        </div>
       ) : null}
-    </div>
+    </>
   );
 }
 
