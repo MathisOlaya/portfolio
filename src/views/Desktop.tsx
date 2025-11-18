240f80589f141f7662248322bcacc7b3ed19e78f
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-11-18 22:13:03 +0100

    feat(desktop): display the background

---
+++ src/views/Desktop.tsx
@@ -0,0 +1,14 @@
+// Components
+import FloatingLines from "../components/FloatingLines";
+
+function Desktop() {
+  return (
+    <>
+      <div className="h-screen">
+        <FloatingLines interactive={false} />
+      </div>
+    </>
+  );
+}
+
+export default Desktop;
