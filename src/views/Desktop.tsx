8cd0c301b3f509786681145e2cbf758d41801ba3
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-11-18 23:10:47 +0100

    feat(desktop): stretch div to whole screen

--- src/views/Desktop.tsx
+++ src/views/Desktop.tsx
@@ -7,7 +7,7 @@ function Desktop() {
     <>
       <div className="h-screen relative">
         <FloatingLines interactive={false} />
-        <main className="absolute flex gap-1 top-0 left-0 py-2">
+        <main className="absolute w-full h-full items-start flex gap-1 top-0 left-0 py-2">
           <Executable icon="/my-pc.png" name="Ce PC" />
           <Executable icon="/folder.png" name="Mon dossier" />
         </main>
