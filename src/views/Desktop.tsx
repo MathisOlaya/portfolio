f9befeaad7152595b55b958ee54c18c2e5111b25
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-11-18 22:47:50 +0100

    feat(exe): add 2 default applications on desktop

--- src/views/Desktop.tsx
+++ src/views/Desktop.tsx
@@ -1,11 +1,16 @@
 // Components
 import FloatingLines from "../components/FloatingLines";
+import Executable from "../components/Executable";
 
 function Desktop() {
   return (
     <>
-      <div className="h-screen">
+      <div className="h-screen relative">
         <FloatingLines interactive={false} />
+        <main className="absolute flex gap-1 top-0 left-0 py-2">
+          <Executable icon="/my-pc.png" name="Ce PC" />
+          <Executable icon="/folder.png" name="Mon dossier" />
+        </main>
       </div>
     </>
   );
