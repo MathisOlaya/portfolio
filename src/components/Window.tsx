eb06a12173b6d237d82e4e281d69142353752137
Mathis Olaya <150446507+MathisOlaya@users.noreply.github.com>
2025-11-19 16:15:11 +0100

    feat(window): add header with close button

--- src/components/Window.tsx
+++ src/components/Window.tsx
@@ -16,5 +16,14 @@ function Window({icon, name, children, onCloseButtonClick} : Props) {
               md:w-2/3 md:max-w-xl
               bg-gray-700
             ">
+                <div  onMouseDown={() => setMouseDownState(true)} onMouseMove={handleMouseMovement} onMouseUp={() => setMouseDownState(false)}  className="flex bg-black justify-between">
+                    <div className="flex gap-2 items-center p-2">
+                        <img className="w-3 object-contain" src={icon}/>
+                        <p>{name}</p>
+                    </div>
+                    <div>
+                        <button onClick={onCloseButtonClick} className="hover:bg-red-500 py-2 aspect-[7/3]" >X</button>
+                    </div>
+                </div>
             {children}
         </div>
