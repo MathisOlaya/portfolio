e37e65d010d5cd4f238afb0494da7c9a7fd09f59
Mathis Olaya <150446507+MathisOlaya@users.noreply.github.com>
2025-11-19 16:14:22 +0100

    feat(window): display content as children

--- src/components/Window.tsx
+++ src/components/Window.tsx
@@ -8,3 +8,13 @@ interface Props {
     onCloseButtonClick: () => void;
 }
 
+function Window({icon, name, children, onCloseButtonClick} : Props) {
+    return (
+        <div id="window" style={style} className="
+              absolute flex flex-col
+              top-[var(--top)] left-[var(--left)] w-full -translate-x-1/2 -translate-y-1/2 aspect-[4/3]
+              md:w-2/3 md:max-w-xl
+              bg-gray-700
+            ">
+            {children}
+        </div>
