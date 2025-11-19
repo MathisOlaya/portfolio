5a42c0a28e9238e4175184bf2f304e97d63cbd30
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-11-19 19:10:15 +0100

    feat(window): make window movable

--- src/components/Window.tsx
+++ src/components/Window.tsx
@@ -1,70 +1,93 @@
-import {useState, type BaseSyntheticEvent, type ReactNode} from "react";
+import { useState, type BaseSyntheticEvent, type ReactNode } from "react";
 
 // Interface PROPS
 interface Props {
-    icon: string;
-    name: string;
-    children: ReactNode;
-    onCloseButtonClick: () => void;
+  icon: string;
+  name: string;
+  children: ReactNode;
+  onCloseButtonClick: () => void;
 }
 
 interface CSSVariables extends React.CSSProperties {
   [key: `--${string}`]: string | number;
 }
 
-function Window({icon, name, children, onCloseButtonClick} : Props) {
-    const [top, setTop] = useState(50)
-    const [left, setLeft] = useState(50)
-    const [mouseDown, setMouseDownState] = useState(false);
+function Window({ icon, name, children, onCloseButtonClick }: Props) {
+  const [top, setTop] = useState(50);
+  const [left, setLeft] = useState(50);
+  const [mouseDown, setMouseDownState] = useState(false);
+  const [offset, setOffset] = useState({ x: 0, y: 0 });
 
-    const style: CSSVariables = {
-        "--top": `${top}%`,
-        "--left": `${left}%`,
-    };
+  const style: CSSVariables = {
+    "--top": `${top}%`,
+    "--left": `${left}%`,
+  };
 
-    const handleMouseMovement = (event : React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-        if(!mouseDown) return;
-        
-        // get div
-        const div = document.getElementById('window');
+  const handleMouseDown = (event: React.MouseEvent<HTMLDivElement>) => {
+    const div = document.getElementById("window");
+    if (!div) return;
 
-        // get pixel distance between top left corner of window AND screen origin
-        const diffX = window.innerWidth / 2 - div?.offsetWidth / 2
-        const diffY = window.innerHeight / 2 - div?.offsetHeight / 2
+    // distance between top left corner of window and mouse position
+    const rect = div.getBoundingClientRect();
+    setOffset({ x: event.clientX - rect.left, y: event.clientY - rect.top });
+    setMouseDownState(true);
+  };
 
-        // get pixel distance between mouse pos and top left corner of window
-        const mouseDiffX = event.clientX - diffX
-        const mouseDiffY = event.clientY - diffY
+  const handleMouseMovement = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    if (!mouseDown) return;
 
-        // calc distance from left and top as percentage
-        const leftPercentage = (diffX + mouseDiffX) / window.innerWidth * 100
-        const topPercentage = (diffY + mouseDiffY) / window.innerHeight * 100
-        
-        
-        setLeft(leftPercentage);
-        setTop(topPercentage);
-    }
+    // get div
+    const div = document.getElementById("window");
 
+    if (!mouseDown) return;
 
-    return (
-        <div id="window" style={style} className="
+    const leftPercentage =
+      ((event.clientX - offset.x + div!.offsetWidth / 2) / window.innerWidth) *
+      100;
+    const topPercentage =
+      ((event.clientY - offset.y + div!.offsetHeight / 2) /
+        window.innerHeight) *
+      100;
+
+    setLeft(leftPercentage);
+    setTop(topPercentage);
+  };
+
+  return (
+    <div
+      id="window"
+      style={style}
+      className="
               absolute flex flex-col
               top-[var(--top)] left-[var(--left)] w-full -translate-x-1/2 -translate-y-1/2 aspect-[4/3]
               md:w-2/3 md:max-w-xl
               bg-gray-700
-            ">
-                <div  onMouseDown={() => setMouseDownState(true)} onMouseMove={handleMouseMovement} onMouseUp={() => setMouseDownState(false)}  className="flex bg-black justify-between">
-                    <div className="flex gap-2 items-center p-2">
-                        <img className="w-3 object-contain" src={icon}/>
-                        <p>{name}</p>
-                    </div>
-                    <div>
-                        <button onClick={onCloseButtonClick} className="hover:bg-red-500 py-2 aspect-[7/3]" >X</button>
-                    </div>
-                </div>
-            {children}
+            "
+    >
+      <div
+        onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMovement}
+        onMouseUp={() => setMouseDownState(false)}
+        className="flex bg-black justify-between"
+      >
+        <div className="flex gap-2 items-center p-2">
+          <img className="w-3 object-contain" src={icon} />
+          <p>{name}</p>
         </div>
-    )
+        <div>
+          <button
+            onClick={onCloseButtonClick}
+            className="hover:bg-red-500 py-2 aspect-[7/3]"
+          >
+            X
+          </button>
+        </div>
+      </div>
+      {children}
+    </div>
+  );
 }
 
 export default Window;
