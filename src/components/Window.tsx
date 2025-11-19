3bc48c47cb6e1fec4cf9f4eb779d17a6cdd91826
Mathis Olaya <150446507+MathisOlaya@users.noreply.github.com>
2025-11-19 16:15:46 +0100

    WIP(window): window deplacement with mouse

--- src/components/Window.tsx
+++ src/components/Window.tsx
@@ -8,7 +8,44 @@ interface Props {
     onCloseButtonClick: () => void;
 }
 
+interface CSSVariables extends React.CSSProperties {
+  [key: `--${string}`]: string | number;
+}
+
 function Window({icon, name, children, onCloseButtonClick} : Props) {
+    const [top, setTop] = useState(50)
+    const [left, setLeft] = useState(50)
+    const [mouseDown, setMouseDownState] = useState(false);
+
+    const style: CSSVariables = {
+        "--top": `${top}%`,
+        "--left": `${left}%`,
+    };
+
+    const handleMouseMovement = (event : React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        if(!mouseDown) return;
+        
+        // get div
+        const div = document.getElementById('window');
+
+        // get pixel distance between top left corner of window AND screen origin
+        const diffX = window.innerWidth / 2 - div?.offsetWidth / 2
+        const diffY = window.innerHeight / 2 - div?.offsetHeight / 2
+
+        // get pixel distance between mouse pos and top left corner of window
+        const mouseDiffX = event.clientX - diffX
+        const mouseDiffY = event.clientY - diffY
+
+        // calc distance from left and top as percentage
+        const leftPercentage = (diffX + mouseDiffX) / window.innerWidth * 100
+        const topPercentage = (diffY + mouseDiffY) / window.innerHeight * 100
+        
+        
+        setLeft(leftPercentage);
+        setTop(topPercentage);
+    }
+
+
     return (
         <div id="window" style={style} className="
               absolute flex flex-col
@@ -27,3 +64,7 @@ function Window({icon, name, children, onCloseButtonClick} : Props) {
                 </div>
             {children}
         </div>
+    )
+}
+
+export default Window;
