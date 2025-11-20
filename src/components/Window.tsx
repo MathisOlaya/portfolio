5195aab8fdcc2ead2ea1ac2e1021a52ce99269ae
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-11-20 22:47:18 +0100

    chore: rename navigator tab name

--- src/components/Window.tsx
+++ src/components/Window.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, type ReactNode } from "react";
+import { useState, useRef, type ReactNode } from 'react';
 
 // Interface PROPS
 interface Props {
@@ -17,17 +17,15 @@ function Window({ icon, name, children, onCloseButtonClick }: Props) {
   const [left, setLeft] = useState(50);
   const [mouseDown, setMouseDownState] = useState(false);
   const [offset, setOffset] = useState({ x: 0, y: 0 });
-  
+
   const windowRef = useRef<HTMLDivElement | null>(null);
 
   const style: CSSVariables = {
-    "--top": `${top}%`,
-    "--left": `${left}%`,
+    '--top': `${top}%`,
+    '--left': `${left}%`,
   };
 
-  const handleDown = (
-    event: React.MouseEvent<HTMLDivElement> | React.TouchEvent
-  ) => {
+  const handleDown = (event: React.MouseEvent<HTMLDivElement> | React.TouchEvent) => {
     const div = windowRef.current;
     if (!div) return;
 
@@ -37,9 +35,7 @@ function Window({ icon, name, children, onCloseButtonClick }: Props) {
     setMouseDownState(true);
   };
 
-  const handleMove = (
-    event: React.MouseEvent<HTMLDivElement, MouseEvent> | React.TouchEvent
-  ) => {
+  const handleMove = (event: React.MouseEvent<HTMLDivElement, MouseEvent> | React.TouchEvent) => {
     if (!mouseDown) return;
 
     const div = windowRef.current;
@@ -47,17 +43,15 @@ function Window({ icon, name, children, onCloseButtonClick }: Props) {
 
     const { x, y } = getClientXY(event);
 
-    const leftPercentage =
-      ((x - offset.x + div.offsetWidth / 2) / window.innerWidth) * 100;
-    const topPercentage =
-      ((y - offset.y + div.offsetHeight / 2) / window.innerHeight) * 100;
+    const leftPercentage = ((x - offset.x + div.offsetWidth / 2) / window.innerWidth) * 100;
+    const topPercentage = ((y - offset.y + div.offsetHeight / 2) / window.innerHeight) * 100;
 
     setLeft(leftPercentage);
     setTop(topPercentage);
   };
 
   const getClientXY = (event: React.MouseEvent | React.TouchEvent) => {
-    if ("touches" in event) {
+    if ('touches' in event) {
       return { x: event.touches[0].clientX, y: event.touches[0].clientY };
     } else {
       return { x: event.clientX, y: event.clientY };
@@ -70,29 +64,18 @@ function Window({ icon, name, children, onCloseButtonClick }: Props) {
       style={style}
       className="
               absolute flex flex-col
-              top-[var(--top)] left-[var(--left)] w-full -translate-x-1/2 -translate-y-1/2 aspect-[4/3]
+              top-[var(--top)] left-[var(--left)] w-full -translate-x-1/2 -translate-y-1/2 aspect-[4/3] touch-none
               md:w-2/3 md:max-w-xl
               bg-gray-700
             "
     >
-      <div
-        onTouchStart={handleDown}
-        onTouchMove={handleMove}
-        onTouchEnd={() => setMouseDownState(false)}
-        onMouseDown={handleDown}
-        onMouseMove={handleMove}
-        onMouseUp={() => setMouseDownState(false)}
-        className="flex bg-black justify-between"
-      >
+      <div onTouchStart={handleDown} onTouchMove={handleMove} onTouchEnd={() => setMouseDownState(false)} onMouseDown={handleDown} onMouseMove={handleMove} onMouseUp={() => setMouseDownState(false)} className="flex bg-black justify-between">
         <div className="flex gap-2 items-center p-2">
           <img className="w-3 object-contain" src={icon} />
           <p>{name}</p>
         </div>
         <div>
-          <button
-            onClick={onCloseButtonClick}
-            className="hover:bg-red-500 py-2 aspect-[7/3]"
-          >
+          <button onClick={onCloseButtonClick} className="hover:bg-red-500 py-2 aspect-[7/3]">
             X
           </button>
         </div>
