51f94e696d32353379a0a00b0c052f8e5ed1a3ac
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-11-21 18:50:37 +0100

    feat(notepad): create component for markdown rendering

---
+++ src/components/Apps/Notepad/Notepad.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactMarkDown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+
+// Interface Props
+interface Props {
+  content: string;
+}
+
+function Notepad({ content }: Props) {
+  return (
+    <div className="prose prose-slate text-slate-900 dark:text-slate-200 prose-headings:text-black prose-li:text-gray-300">
+      <ReactMarkDown remarkPlugins={[remarkGfm]}>{content}</ReactMarkDown>
+    </div>
+  );
+}
+
+export default Notepad;
