fab4e6ab8e470f3359a7054dd0c09fe6846ee819
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-11-21 19:12:12 +0100

    chore: add padding to notepad

--- src/components/Apps/Notepad/Notepad.tsx
+++ src/components/Apps/Notepad/Notepad.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 function Notepad({ content }: Props) {
   return (
-    <div className="prose prose-slate text-slate-900 dark:text-slate-200 prose-headings:text-black prose-li:text-gray-300">
+    <div className="prose prose-slate text-slate-900 dark:text-slate-200 prose-headings:text-black prose-li:text-gray-300 p-4">
       <ReactMarkDown remarkPlugins={[remarkGfm]}>{content}</ReactMarkDown>
     </div>
   );
