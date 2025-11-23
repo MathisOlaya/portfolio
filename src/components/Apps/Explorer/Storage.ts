1836c6b6805c2428584b6602484ec5c23040390f
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-11-23 17:04:13 +0100

    chore(storage): update storage content

--- src/components/Apps/Explorer/Storage.ts
+++ src/components/Apps/Explorer/Storage.ts
@@ -44,7 +44,7 @@ export const Storage: Folder = {
                 {
                   name: 'README.md',
                   type: 'file',
-                  content: "# Bienvenu(e) \n Vous Retrouverez dans ce dossier tout mes projets publique également disponibles sur GitHub. \n ## Qui suis-je ? \n Je m'appelle Mathis Olaya et je suis étudiant en 3ème année de développement en **Informatique** à l'école des métiers à *Lausanne*. Autant sur le plan scolaire que personnel, j'ai eu l'occasion d'acquérir des compétences dans plusieurs domaines. Notemment ceux ci : \n - Web Development \n - Software Development",
+                  content: "# Bienvenu(e) \n Vous Retrouverez dans ce dossier tous mes projets publiques également disponibles sur [GitHub](https://github.com/mathisolaya). \n ## Qui suis-je ? \n Je m'appelle Mathis Olaya et je suis étudiant en 3ème année de développement en **Informatique** à l'école des métiers à *Lausanne*. Autant sur le plan scolaire que personnel, j'ai eu l'occasion d'acquérir des compétences dans plusieurs domaines. Notemment ceux ci : \n - Web Development \n - Software Development \n - Databases (SQL - NoSQL)",
                 },
               ],
             },
