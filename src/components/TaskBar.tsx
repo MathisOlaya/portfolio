eaadd2802d35cef0c350af85197bc76850543a83
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-11-23 16:21:14 +0100

    feat(time): display local time and a update it every sec

--- src/components/TaskBar.tsx
+++ src/components/TaskBar.tsx
@@ -1,28 +1,45 @@
 // Provider
 import { useAppManager, type AppInstance } from '../managers/AppManager';
 
+// Helpers
+import DateHelper from '../helpers/DateHelper';
+
 // Icons
 import { FaWindows } from 'react-icons/fa';
+import { useState } from 'react';
 
 function TaskBar() {
   const { openApps, toggleMinimization } = useAppManager();
+
+  // State
+  const [time, setTime] = useState<String>(DateHelper.getTime(new Date()));
+  // const [date, setDate] = useState<String>(DateHelper.getDate(new Date()));
 
   const toggleAppMinimize = (app: AppInstance) => {
     toggleMinimization(app.id);
   };
 
+  setInterval(() => {
+    setTime(DateHelper.getTime(new Date()));
+  }, 2000);
+
   return (
-    <div className="absolute flex bottom-0 w-full bg-gray-500">
-      <FaWindows size={24} className="mr-4 m-3" />
-      <div className="gap-1 flex">
-        {openApps.map((app) => {
-          return (
-            <div onClick={() => toggleAppMinimize(app)} key={app.id} className="flex align-contents-center gap-2 hover:bg-gray-800 p-3">
-              <img className="w-4" src={app.icon} alt="" />
-              <p>{app.name}</p>
-            </div>
-          );
-        })}
+    <div className="absolute flex bottom-0 w-full bg-gray-500 justify-between items-center">
+      <div className="flex">
+        <FaWindows size={24} className="mr-4 m-3" />
+        <div className="gap-1 flex">
+          {openApps.map((app) => {
+            return (
+              <div onClick={() => toggleAppMinimize(app)} key={app.id} className="flex align-contents-center gap-2 hover:bg-gray-800 p-3">
+                <img className="w-4" src={app.icon} alt="" />
+                <p>{app.name}</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div className="mr-4 m-3">
+        <p>{time}</p>
       </div>
     </div>
   );
