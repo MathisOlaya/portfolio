e02bf9a30fa70cadb4fef4c6ed3deb818f10e63a
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-11-23 16:04:06 +0100

    feat(notepad): add "notepad" to application name if application is notepad

--- src/managers/AppManager.tsx
+++ src/managers/AppManager.tsx
@@ -1,10 +1,10 @@
-import { createContext, useContext, useState, type ReactNode } from 'react';
+import { createContext, useContext, useState, isValidElement, type ReactNode } from 'react';
 
 export interface AppInstance {
   id: string;
   name: string;
   icon: string;
-  position: {top: number, left: number}
+  position: { top: number; left: number };
   content: ReactNode;
   minimized: boolean;
 }
@@ -13,7 +13,7 @@ interface AppManagerContextType {
   openApps: AppInstance[];
   openApp: (name: string, icon: string, content: ReactNode) => void;
   closeApp: (id: string) => void;
-  moveApp: (id: string, newPos: {top: number, left: number}) => void;
+  moveApp: (id: string, newPos: { top: number; left: number }) => void;
   toggleMinimization: (id: string) => void;
 }
 
@@ -23,6 +23,10 @@ export const AppManagerProvider = ({ children }: { children: ReactNode }) => {
   const [openApps, setOpenApps] = useState<AppInstance[]>([]);
 
   const openApp = (name: string, icon: string, content: ReactNode) => {
+    if (isValidElement(content) && content.type && (content.type as any).name === 'Notepad') {
+      name = `Notepad - ${name}`;
+    }
+
     setOpenApps((prev) => [
       ...prev,
       {
@@ -30,36 +34,23 @@ export const AppManagerProvider = ({ children }: { children: ReactNode }) => {
         name,
         icon,
         content,
-        position: {top: 50, left: 50},
+        position: { top: 50, left: 50 },
         minimized: false,
       },
     ]);
   };
 
-  const moveApp = (id: string, newPos: {top: number, left:number}) => {
-    setOpenApps(prev =>
-      prev.map(app =>
-        app.id === id
-          ? { ...app, position: newPos } 
-          : app
-      )
-    );
-  }
+  const moveApp = (id: string, newPos: { top: number; left: number }) => {
+    setOpenApps((prev) => prev.map((app) => (app.id === id ? { ...app, position: newPos } : app)));
+  };
 
   const closeApp = (id: string) => {
     setOpenApps((prev) => prev.filter((app) => app.id !== id));
   };
 
   const toggleMinimization = (id: string) => {
-    setOpenApps(prev =>
-      prev.map(app =>
-        app.id === id
-          ? { ...app, minimized: !app.minimized } 
-          : app
-      )
-    );
-};
-
+    setOpenApps((prev) => prev.map((app) => (app.id === id ? { ...app, minimized: !app.minimized } : app)));
+  };
 
   return <AppManagerContext.Provider value={{ openApps, openApp, closeApp, moveApp, toggleMinimization }}>{children}</AppManagerContext.Provider>;
 };
